d5dcf411cdc17f5a925660ea238f2d8f97c48eaa
Hannah <hyoung20@gmail.com>
2013-09-27 15:19:10 -0700

    Added bird sighting form layout and action bar icons for camera and map. Signed-off-by: Hannah <hyoung20@gmail.com>
    	new file:   res/drawable-hdpi/ic_camera2.png
    	new file:   res/drawable-hdpi/ic_map2.png
    	new file:   res/drawable-ldpi/ic_camera2.png
    	new file:   res/drawable-ldpi/ic_map2.png
    	new file:   res/drawable-mdpi/ic_camera2.png
    	new file:   res/drawable-mdpi/ic_map2.png
    	new file:   res/drawable-xhdpi/ic_camera2.png
    	new file:   res/drawable-xhdpi/ic_map2.png
    	modified:   res/layout/activity_bird_form.xml
    	modified:   res/menu/bird_form.xml
    	modified:   res/values/strings.xml
    	modified:   src/com/catalyst/android/birdapp/BirdFormActivity.java

--- src/com/catalyst/android/birdapp/BirdFormActivity.java
+++ src/com/catalyst/android/birdapp/BirdFormActivity.java
@@ -2,17 +2,32 @@ package com.catalyst.android.birdapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+
 import android.content.Intent;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
 public class BirdFormActivity extends Activity {
+	
+	Spinner categorySpinner;
+	Spinner activitySpinner;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bird_form);
-		Intent intent = new Intent(getApplication(), Map_Activity.class);
-		startActivity(intent);
+		categorySpinner = (Spinner) findViewById(R.id.category_drop_down);
+		activitySpinner = (Spinner) findViewById(R.id.bird_acivity_dropdown);
+		TextView textView = (TextView)findViewById(R.id.notes_edit_text);
+		textView.setMovementMethod(ScrollingMovementMethod.getInstance());
+		populateSpinners();
+
 	}
 
 	@Override
@@ -21,5 +36,28 @@ public class BirdFormActivity extends Activity {
 		getMenuInflater().inflate(R.menu.bird_form, menu);
 		return true;
 	}
+	
 
+	
+	public void populateSpinners(){
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(BirdFormActivity.this,
+		        R.array.category_drop_down_choices, android.R.layout.simple_spinner_item);
+		
+		ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(BirdFormActivity.this,
+		        R.array.bird_acivity_dropdown_choices, android.R.layout.simple_spinner_item);
+		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		categorySpinner.setAdapter(adapter);
+		activitySpinner.setAdapter(activityAdapter);
+		
+	}
+	
+
+	
+	public void getMap(View view){
+		Intent intent = new Intent(getApplication(), Map_Activity.class);
+		startActivity(intent);
+	}
 }
