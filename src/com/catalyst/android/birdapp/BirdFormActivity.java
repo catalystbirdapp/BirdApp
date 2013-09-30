c28e1777ffb9c20f3b7b74e125f5fe6c6b8e1d3f
Hannah <hyoung20@gmail.com>
2013-09-30 11:24:27 -0700

    Changed lat and long fields to plain text field so that they accept decimals and negatives. Added date(DD/MM/YYYY) and time(HH:MM) fields to bird sighting form.
    Signed-off-by: Hannah <hyoung20@gmail.com>
    
    	modified:   res/layout/activity_bird_form.xml
    	modified:   res/values/strings.xml
    	modified:   src/com/catalyst/android/birdapp/BirdFormActivity.java

--- src/com/catalyst/android/birdapp/BirdFormActivity.java
+++ src/com/catalyst/android/birdapp/BirdFormActivity.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,6 +16,7 @@ public class BirdFormActivity extends Activity {
 	
 	Spinner categorySpinner;
 	Spinner activitySpinner;
+	Spinner amPmSpinner;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +24,7 @@ public class BirdFormActivity extends Activity {
 		setContentView(R.layout.activity_bird_form);
 		categorySpinner = (Spinner) findViewById(R.id.category_drop_down);
 		activitySpinner = (Spinner) findViewById(R.id.bird_acivity_dropdown);
+		amPmSpinner = (Spinner) findViewById(R.id.am_pm_spinner);
 		TextView textView = (TextView)findViewById(R.id.notes_edit_text);
 		textView.setMovementMethod(ScrollingMovementMethod.getInstance());
 		populateSpinners();
@@ -41,15 +41,21 @@ public class BirdFormActivity extends Activity {
 
 	
 	public void populateSpinners(){
+		
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(BirdFormActivity.this,
 		        R.array.category_drop_down_choices, android.R.layout.simple_spinner_item);
 		
 		ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(BirdFormActivity.this,
 		        R.array.bird_acivity_dropdown_choices, android.R.layout.simple_spinner_item);
 		
+		ArrayAdapter<CharSequence> amPmAdapter = ArrayAdapter.createFromResource(BirdFormActivity.this,
+		        R.array.am_pm_choices, android.R.layout.simple_spinner_item);
+		
+		amPmAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		
+		amPmSpinner.setAdapter(amPmAdapter);
 		categorySpinner.setAdapter(adapter);
 		activitySpinner.setAdapter(activityAdapter);
 		
