b1c73d408a36cc606689043761d47481e3f7e3f8
Mike Howell <mhowell@catalystitservices.com>
2013-10-09 11:19:20 -0700

    Added custom spinner item, added a style for spinner text, prompts for the spinners, etc.

--- src/com/catalyst/android/birdapp/BirdFormActivity.java
+++ src/com/catalyst/android/birdapp/BirdFormActivity.java
@@ -120,14 +120,14 @@ public class BirdFormActivity extends Activity implements android.view.View.OnCl
 	private void fillActivitySpinner() {
 		DatabaseHandler dbHandler = DatabaseHandler.getInstance(this);
 		ArrayList<String> activitiesFromDB = dbHandler.getAllActivities();
-		ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, activitiesFromDB);
+		ArrayAdapter adapter = new ArrayAdapter(this, R.layout.spinner_item, R.id.spinnertextview, activitiesFromDB);
 		activitySpinner.setAdapter(adapter);
 		
 	}
 	private void fillCategorySpinner() {
 		DatabaseHandler dbHandler = DatabaseHandler.getInstance(this);
 		ArrayList<String> categoriesFromDB = dbHandler.getAllCategories();
-		ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, categoriesFromDB);
+		ArrayAdapter adapter = new ArrayAdapter(this, R.layout.spinner_item, R.id.spinnertextview, categoriesFromDB);
 		categorySpinner.setAdapter(adapter);
 		
 	}
