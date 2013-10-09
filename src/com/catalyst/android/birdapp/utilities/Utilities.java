489dedf4877e933304162afd7668e91853115670
Mike Howell <mhowell@catalystitservices.com>
2013-10-09 08:58:19 -0700

    Working on enhancing the appearance of the spinners

--- src/com/catalyst/android/birdapp/utilities/Utilities.java
+++ src/com/catalyst/android/birdapp/utilities/Utilities.java
@@ -1,63 +1,87 @@
 package com.catalyst.android.birdapp.utilities;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import java.text.ParseException;
-
 public class Utilities {
-	
+
 	private final static String DATE_TIME_FORMAT_ENGLISH = "MM/dd/yyyy' 'HH:mm a";
 	private final static String DATE_TIME_FORMAT_GERMAN = "dd/MM/yyyy' 'HH:mm";
 
+	/**
+	 * Returns the current time in milliseconds
+	 * 
+	 * @return
+	 */
 	public long currentMillis() {
 		long currentTime = System.currentTimeMillis();
 		return currentTime;
 	}
 
-	public String formatDate(long millis){
+	/**
+	 * Formats the date for human readable display. Different depending on
+	 * locale.
+	 * 
+	 * @param millis
+	 * @return formatted date
+	 */
+	public String formatDate(long millis) {
 		String countryCode = getLocaleCode();
-		DateFormat formatter;   
-		if (countryCode.equals("DE")){
+		DateFormat formatter;
+		if (countryCode.equals("DE")) {
 			formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.GERMAN);
 		} else {
 			formatter = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
 		}
-		
+
 		return formatter.format(millis);
 	}
 
-	public String formatTime(long millis){
+	/**
+	 * Formats the time for human readable display. Different depending on
+	 * locale.
+	 * 
+	 * @param millis
+	 * @return formatted time
+	 */
+	public String formatTime(long millis) {
 		String countryCode = getLocaleCode();
 		DateFormat formatter;
-		if (countryCode.equals("DE")){
+		if (countryCode.equals("DE")) {
 			formatter = new SimpleDateFormat("HH:mm", Locale.GERMAN);
 		} else {
 			formatter = new SimpleDateFormat("hh:mm a", Locale.ENGLISH);
 		}
-		
+
 		return formatter.format(millis);
 	}
-	
-	protected String getLocaleCode(){
+
+	/**
+	 * Gets the locale that the device has been set to
+	 * 
+	 * @return two digit country code
+	 */
+	protected String getLocaleCode() {
 		Locale locale = Locale.getDefault();
 		String country = locale.getCountry();
 		return country;
 	}
-	
-	
+
 	public Date getDateObject(String dateString) {
 		String countryCode = getLocaleCode();
 		DateFormat formatter;
-		if (countryCode.equals("DE")){
-			formatter = new SimpleDateFormat(DATE_TIME_FORMAT_GERMAN, Locale.GERMAN);
+		if (countryCode.equals("DE")) {
+			formatter = new SimpleDateFormat(DATE_TIME_FORMAT_GERMAN,
+					Locale.GERMAN);
 		} else {
-			formatter = new SimpleDateFormat(DATE_TIME_FORMAT_ENGLISH, Locale.ENGLISH);
+			formatter = new SimpleDateFormat(DATE_TIME_FORMAT_ENGLISH,
+					Locale.ENGLISH);
 		}
 		try {
-			Date d = (Date)formatter.parse(dateString);
+			Date d = (Date) formatter.parse(dateString);
 			return d;
 		} catch (ParseException e) {
 			e.printStackTrace();
