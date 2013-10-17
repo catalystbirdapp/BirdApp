fc54e192369036e3cc777433528946ac946e0fbe
Hannah <hyoung20@gmail.com>
2013-10-17 14:33:29 -0700

    Added comments, cleaned up code.
    Signed-off-by: Hannah <hyoung20@gmail.com>
    
    	modified:   AndroidManifest.xml
    	modified:   res/layout/activity_camera_layout.xml
    	modified:   res/layout/camera_controls.xml
    	modified:   res/values/strings.xml
    	modified:   src/com/catalyst/android/birdapp/CameraActivity.java
    	modified:   src/com/catalyst/android/birdapp/camera/CameraPreview.java

--- src/com/catalyst/android/birdapp/CameraActivity.java
+++ src/com/catalyst/android/birdapp/CameraActivity.java
@@ -1,26 +1,15 @@
 package com.catalyst.android.birdapp;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import com.catalyst.android.birdapp.camera.CameraPreview;
-
 import android.app.Activity;
 import android.hardware.Camera;
-import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+
 
 public class CameraActivity extends Activity {
 
@@ -28,6 +17,7 @@ public class CameraActivity extends Activity {
     private CameraPreview mCameraPreview;
 
     /** Called when the activity is first created. */
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +25,15 @@ public class CameraActivity extends Activity {
         mCamera = getCameraInstance();
         mCameraPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mCameraPreview);
+        preview.addView(mCameraPreview); //calls CameraPreview class which starts the preview(aka the camera display)
         RelativeLayout relativeLayoutControls = (RelativeLayout) findViewById(R.id.controls_layout);
-        relativeLayoutControls.bringToFront();
+        relativeLayoutControls.bringToFront(); //used to bring the capture button the front so that it overlays the preview display
         
         Button captureButton = (Button) findViewById(R.id.button_capture);
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCamera.takePicture(null, null, mPicture);
+               //TODO implement saving photo on click here
             }
         });
     }
@@ -59,53 +49,11 @@ public class CameraActivity extends Activity {
         try {
             camera = Camera.open();
         } catch (Exception e) {
-            // cannot get camera or does not exist
+            
         }
         return camera;
     }
 
-    PictureCallback mPicture = new PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            File pictureFile = getOutputMediaFile();
-            if (pictureFile == null) {
-                return;
-            }
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-                mCamera.startPreview();
-            } catch (FileNotFoundException e) {
-
-            } catch (IOException e) {
-            }
-         
-        }
-       
-    };
-
-    private static File getOutputMediaFile() {
-        File mediaStorageDir = new File(
-                Environment
-                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                "MyCameraApp");
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
-                
-                return null;
-            }
-        }
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-                .format(new Date());
-        File mediaFile;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator
-                + "IMG_" + timeStamp + ".jpg");
-
-        return mediaFile;
-    }
 	
 	
 }
--- src/com/catalyst/android/birdapp/camera/CameraPreview.java
+++ src/com/catalyst/android/birdapp/camera/CameraPreview.java
@@ -12,108 +12,110 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 
 
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+
+public class CameraPreview extends SurfaceView implements
+		SurfaceHolder.Callback {
+
 	private boolean isPreviewRunning;
-	 private SurfaceHolder mSurfaceHolder;
-	    private Camera mCamera;
-	    private Context context;
-	    // Constructor that obtains context and camera
-	  
-	    public CameraPreview(Context context, Camera camera) {
-	        super(context);
-	        this.context = context;
-	        this.mCamera = camera;
-	        this.mSurfaceHolder = this.getHolder();
-	        this.mSurfaceHolder.addCallback(this);
-	     
-	    }
-/**
- * creates surface, starts the preview
- */
-	    @Override
-	    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-	        try {
-	            mCamera.setPreviewDisplay(surfaceHolder);
-	            mCamera.startPreview();
-	        } catch (IOException e) {
-	            // left blank for now
-	        }
-	    }
-/**
- * stops preview and releases camera
- */
-	    @Override
-	    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-	        mCamera.stopPreview();
-	        mCamera.release();
-	    }
+	private SurfaceHolder mSurfaceHolder;
+	private Camera mCamera;
+	private Context context;
 
-	    /**
-	     * on surface change (screen rotation), changes preview display
-	     */
-	    @Override
-	    public void surfaceChanged(SurfaceHolder surfaceHolder, int format,
-	            int width, int height) {
-	    	
-	    	if (isPreviewRunning)
-	        {
-	            mCamera.stopPreview();
-	        }
+	// Constructor that obtains context and camera
+	public CameraPreview(Context context, Camera camera) {
+		super(context);
+		this.context = context;
+		this.mCamera = camera;
+		this.mSurfaceHolder = this.getHolder();
+		this.mSurfaceHolder.addCallback(this);
 
-	        Parameters parameters = mCamera.getParameters();
-	     
-	        Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+	}
 
-	        if(display.getRotation() == Surface.ROTATION_0)
-	        {
-	                                    
-	            mCamera.setDisplayOrientation(90);
-	        }
+	/**
+	 * creates surface, starts the camera preview
+	 */
+	@Override
+	public void surfaceCreated(SurfaceHolder surfaceHolder) {
+		try {
+			mCamera.setPreviewDisplay(surfaceHolder);
+			mCamera.startPreview();
+		} catch (IOException e) {
 
-	        if(display.getRotation() == Surface.ROTATION_90)
-	        {
-	            parameters.setPreviewSize(width, height);
-	           
-	        }
+		}
+	}
 
-	        if(display.getRotation() == Surface.ROTATION_180)
-	        {
-	            parameters.setPreviewSize(height, width);  
-	          
-	        }
+	/**
+	 * stops capturing the preview and resets the camera then disconnects and
+	 * releases the camera object.
+	 */
+	@Override
+	public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+		mCamera.stopPreview();
+		mCamera.release();
+	}
 
-	        if(display.getRotation() == Surface.ROTATION_270)
-	        {
-	            parameters.setPreviewSize(width, height);
-	            mCamera.setDisplayOrientation(90);
-	           
-	        }
-        
-	        try {
-				mCamera.setPreviewDisplay(mSurfaceHolder);
-			} catch (IOException e) {
-				
-				e.printStackTrace();
-			}
-	    
-	        previewCamera(); 
-	    }
-	    
-	    /**
-	     * sets camera preview
-	     */
-	  public void previewCamera(){
-	
-		  try 
-		    {           
-		        mCamera.setPreviewDisplay(mSurfaceHolder);          
-		        mCamera.startPreview();
-		        isPreviewRunning = true;
-		    }
-		    catch(Exception e)
-		    {
-		        Log.d("message", "Cannot start preview", e);    
-		    }
-	  }
-	    
+	/**
+	 * on surface change this stops the preview if it's running and then checks
+	 * if the screen has been rotated then calls the previewCamera method to
+	 * restart the preview.
+	 * 
+	 */
+	@Override
+	public void surfaceChanged(SurfaceHolder surfaceHolder, int format,
+			int width, int height) {
+
+		if (isPreviewRunning) {
+			mCamera.stopPreview();
+		}
+
+		Parameters parameters = mCamera.getParameters();
+
+		Display display = ((WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+		if (display.getRotation() == Surface.ROTATION_0) {
+
+			mCamera.setDisplayOrientation(90);
+		}
+
+		if (display.getRotation() == Surface.ROTATION_90) {
+			parameters.setPreviewSize(width, height);
+
+		}
+
+		if (display.getRotation() == Surface.ROTATION_180) {
+			parameters.setPreviewSize(height, width);
+
+		}
+
+		if (display.getRotation() == Surface.ROTATION_270) {
+			parameters.setPreviewSize(width, height);
+			mCamera.setDisplayOrientation(90);
+
+		}
+
+		try {
+			mCamera.setPreviewDisplay(mSurfaceHolder);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		previewCamera();
+	}
+
+	/**
+	 * sets the camera preview display and then starts the preview.
+	 */
+	public void previewCamera() {
+
+		try {
+			mCamera.setPreviewDisplay(mSurfaceHolder);
+			mCamera.startPreview();
+			isPreviewRunning = true;
+		} catch (Exception e) {
+			Log.d("message", "Cannot start preview", e);
+		}
+	}
+
 }
