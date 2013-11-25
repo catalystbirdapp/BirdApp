c49fd15f1fd434598b080903f57fa45a85bc8ae7
maroon5five <randy_miller@mail.com>
2013-11-25 14:39:38 -0800

    Removed the check for google play services as it is not needed. Fix a bug where the manifest and strings file did not get updated last time.
    Signed-off-by: maroon5five <randy_miller@mail.com>
    
    	modified:   AndroidManifest.xml
    	modified:   res/values/strings.xml
    	modified:   src/com/catalyst/android/birdapp/BirdFormActivity.java

--- src/com/catalyst/android/birdapp/BirdFormActivity.java
+++ src/com/catalyst/android/birdapp/BirdFormActivity.java
@@ -78,9 +78,6 @@ public class BirdFormActivity extends Activity implements OnDialogDoneListener {
 		this.getWindow().setSoftInputMode(
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 		setContentView(R.layout.activity_bird_form);
-		// Checks to see if the device has google play services.
-		GooglePlayServicesUtil
-				.isGooglePlayServicesAvailable(getApplicationContext());
 		// Sets up the GPS Utility class
 		gpsUtility = new GPSUtility(this);
 		categorySpinner = (Spinner) findViewById(R.id.category_drop_down);
