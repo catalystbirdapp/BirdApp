3c202b26f0ad33be1a780a1ef972eb3a27e03f44
maroon5five <randy_miller@mail.com>
2013-12-20 09:14:24 -0800

    Fixed camera preview to show fullscreen image and stay in landscape view.
    Signed-off-by: maroon5five <randy_miller@mail.com>
    
    	modified:   AndroidManifest.xml
    	modified:   res/layout/activity_camera_settings.xml
    	modified:   res/layout/activity_picture_confirmation.xml
    	modified:   res/layout/camera_controls.xml
    	modified:   res/values/dimens.xml
    	modified:   src/com/catalyst/android/birdapp/PictureConfirmationActivity.java

--- src/com/catalyst/android/birdapp/PictureConfirmationActivity.java
+++ src/com/catalyst/android/birdapp/PictureConfirmationActivity.java
@@ -1,16 +1,17 @@
 package com.catalyst.android.birdapp;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class PictureConfirmationActivity extends Activity {
@@ -19,23 +20,34 @@ public class PictureConfirmationActivity extends Activity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
+                requestWindowFeature(Window.FEATURE_NO_TITLE);
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 Intent i = getIntent();
                 setContentView(R.layout.activity_picture_confirmation);
                 imageUri = i.getStringExtra("fileName");
 
-                File image = new File(imageUri).getAbsoluteFile();
-                FileInputStream fis = null;
-                try {
-                        fis = new FileInputStream(image);
-                } catch (FileNotFoundException e) {
-
-                        e.printStackTrace();
-                }
-                Bitmap bmp = BitmapFactory.decodeStream(fis);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                ImageView img = (ImageView) findViewById(R.id.image);
-                img.setImageBitmap(bmp);
+                File imgFile = new  File(imageUri);
+				if(imgFile.exists()){
+					//Checks the size of the photo
+					BitmapFactory.Options options = new BitmapFactory.Options();
+					options.inJustDecodeBounds = true;
+					BitmapFactory.decodeFile(imgFile.getAbsolutePath(), options);
+					float srcHeight = options.outHeight;
+					//Gets the screen size
+					Display display = getWindowManager().getDefaultDisplay();
+					Point size = new Point();
+					display.getSize(size);
+					int width = size.x;
+					int height = size.y;
+					//Sets the sample size so that the app can load a sampled down version of the photo to conserve memory
+					int sampleSize = Math.round(srcHeight / height);
+					options = new BitmapFactory.Options();
+					options.inSampleSize = sampleSize;
+					//Gets the image and places it in the map info window at a scaled size for conformity
+				    Bitmap bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath(), options), width, height, false);
+				    ImageView img = (ImageView) findViewById(R.id.image);
+	                img.setImageBitmap(bitmap);
+				}           
         }
 
         // Deletes image from phone.
